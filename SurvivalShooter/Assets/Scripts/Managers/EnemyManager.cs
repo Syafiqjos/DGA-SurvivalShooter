6728e182f6fc361bc68ce4cc755759cab6e0d1fb
Syafiqjos <syafiqjos@gmail.com>
2021-05-19 17:15:06 +0700

    Add Enemy Factory

--- SurvivalShooter/Assets/Scripts/Managers/EnemyManager.cs
+++ SurvivalShooter/Assets/Scripts/Managers/EnemyManager.cs
@@ -3,17 +3,18 @@
 public class EnemyManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
-    public GameObject enemy;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
+    [SerializeField]
+    MonoBehaviour factory;
+    IFactory Factory { get { return factory as IFactory; } }
 
     void Start()
     {
         //Mengeksekusi fungs Spawn setiap beberapa detik sesui dengan nilai spawnTime
         InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
-
 
     void Spawn()
     {
@@ -25,8 +26,9 @@ public class EnemyManager : MonoBehaviour
 
         //Mendapatkan nilai random
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int spawnEnemy = Random.Range(0, 3);
 
         //Memduplikasi enemy
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Factory.FactoryMethod(spawnEnemy);
     }
 }
