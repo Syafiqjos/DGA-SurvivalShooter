4ecb058299e3e93891d1764fbdf3d45b8a1fa71e
Syafiqjos <syafiqjos@gmail.com>
2021-05-19 19:10:24 +0700

    Add New Mechanic Power Up

---
+++ SurvivalShooter/Assets/Scripts/PowerUp/PowerAddHp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerAddHp : MonoBehaviour
+{
+    public int healthIncrease = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            PlayerHealth ph = collision.collider.GetComponent<PlayerHealth>();
+            ph.currentHealth += healthIncrease;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ SurvivalShooter/Assets/Scripts/PowerUp/PowerAddSpeed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerAddSpeed : MonoBehaviour
+{
+    public int movementIncrease = 2;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            PlayerMovement pm = collision.collider.GetComponent<PlayerMovement>();
+            pm.speed += movementIncrease;
+            Destroy(gameObject);
+        }
+    }
+}
